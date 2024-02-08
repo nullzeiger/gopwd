ee9cb95b0bf84f8b8b5462eb96a8f2af1d959302
nullzeiger <ivan.guerreschi.dev@gmail.com>
2024-02-08 16:46:51 +0100

    Add package for file handling and setup GitHub actions

---
+++ cmd/gopwd/main.go
@@ -0,0 +1,12 @@
+// Copyright 2022 Ivan Guerreschi <ivan.guerreschi.dev@gmail.com>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
